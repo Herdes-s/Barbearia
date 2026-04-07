b5a7ba2554d5687ef79fa4753ff47c0b06375e98
herdes <soaressantos02000@gmail.com>
2026-04-07 19:21:07 -0300

    ajuste de texto e fonts

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import styles from "./Header.module.css"
 
+import menu from "../../assets/menu.svg"
+
 function Header() {
     return (
         <header className={styles.header}>
@@ -11,6 +13,7 @@ function Header() {
                     <a href="#preco">Preços</a>
                     <a href="#contato">Contato</a>
                 </nav>
+                <div className={styles.menu}><img src={menu} alt="menu" /></div>
             </div>
         </header>
     )
