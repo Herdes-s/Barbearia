aaf294353c860cf216c83476eb4709f8e072f8b8
herdes <soaressantos02000@gmail.com>
2026-04-10 19:29:58 -0300

    ajustes menu, scroll e outros

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,22 +1,36 @@
-import styles from "./Header.module.css"
+import styles from "./Header.module.css";
 
-import menu from "../../assets/menu.svg"
+import menu from "../../assets/menu.svg";
+import { useState } from "react";
 
 function Header() {
-    return (
-        <header className={styles.header}>
-            <div className={styles.sec_header}>
-                <h1 className={styles.logo}>O Cavalheiro</h1>
-                <nav className={styles.nav}>
-                    <a href="#home">Home</a>
-                    <a href="#sobre">Sobre</a>
-                    <a href="#preco">Preços</a>
-                    <a href="#contato">Contato</a>
-                </nav>
-                <div className={styles.menu}><img src={menu} alt="menu" /></div>
-            </div>
-        </header>
-    )
+  const [open, setOpen] = useState(false);
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.sec_header}>
+        <div className={styles.overlay}></div>
+        <h1 className={styles.logo}>O Cavalheiro</h1>
+        <nav className={`${styles.nav} ${open ? styles.open : ""}`}>
+          <a href="#home" onClick={() => setOpen(false)}>
+            Home
+          </a>
+          <a href="#sobre" onClick={() => setOpen(false)}>
+            Sobre
+          </a>
+          <a href="#servicos" onClick={() => setOpen(false)}>
+            Serviços
+          </a>
+          <a href="#contato" onClick={() => setOpen(false)}>
+            Contato
+          </a>
+        </nav>
+        <div className={`${styles.menu}`} onClick={() => setOpen(!open)}>
+          <img src={menu} alt="menu" />
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -1,19 +1,21 @@
 import Header from "../Header";
-import seta from "../../assets/Arrow1.svg"
+import seta from "../../assets/Arrow1.svg";
 
-import styles from "./Hero.module.css"
+import styles from "./Hero.module.css";
 
 function Hero() {
-    return (
-        <section className={styles.sectionHero} id="home">
-            <div className={styles.overlay}></div>
-            <Header />
-            <div className={styles.info}>
-                <h2 className={styles.tagline}>SEU ESTILO COMEÇA AQUI</h2>
-                <button className={styles.btn}>Vejá agenda <img src={seta} alt="seta" /></button>
-            </div>
-        </section>
-    )
+  return (
+    <section className={styles.sectionHero} id="home">
+      <div className={styles.overlay}></div>
+      <Header />
+      <div className={styles.info}>
+        <h2 className={styles.tagline}>SEU ESTILO COMEÇA AQUI</h2>
+        <button className={styles.btn}>
+          <a href="#servicos">Vejá nossos Serviços</a> <img src={seta} alt="seta" />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
